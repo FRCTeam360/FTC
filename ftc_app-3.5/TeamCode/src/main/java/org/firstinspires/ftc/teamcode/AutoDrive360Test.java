11036e57a49265dd6f8da6023b4d17226ef4f70c
Gavin <kerrgv@gmail.com>
2017-11-15 08:17:36 -0800

    The Drive Function
    I have made it so all directions can be selected.
    
    I plan on making it so it can do the math to figure out distance, but i need to get some measurments first

--- ftc_app-3.5/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDrive360Test.java
+++ ftc_app-3.5/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDrive360Test.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.widget.Switch;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -13,6 +15,7 @@ import static android.os.SystemClock.sleep;
 
 /**
  * Created by Gavin on 11/14/2017.
+ * I just want to say that the way that java handles strings is absolute crap.
  */
 
 @Autonomous(name="Auto Drive 360 Test", group="Linear Opmode")
@@ -33,6 +36,141 @@ public class AutoDrive360Test extends OpMode {
     Servo rightArm;
     Servo jemArm;
 
+    public void telemetryStatus(int choice){
+        if (choice == 1) {
+            telemetry.addLine("Current Instruction: Left");
+            telemetry.update();
+        } else if (choice == 2) {
+            telemetry.addLine("Current Instruction: Right");
+            telemetry.update();
+        } else if (choice == 3) {
+            telemetry.addLine("Current Instruction: Forward");
+            telemetry.update();
+        } else if (choice == 4) {
+            telemetry.addLine("Current Instruction: Backwards");
+            telemetry.update();
+        } else if (choice == 5) {
+            telemetry.addLine("Current Instruction: Left Angle");
+            telemetry.update();
+        } else if (choice == 6) {
+            telemetry.addLine("Current Instruction: Right Angle");
+            telemetry.update();
+        } else if (choice == 7) {
+            telemetry.addLine("Current Instruction: Forward Angle");
+            telemetry.update();
+        } else if (choice == 8) {
+            telemetry.addLine("Current Instruction: Backwards Angle");
+            telemetry.update();
+        } else {
+            telemetry.addLine("You can not have a null direction");
+            telemetry.update();
+        }
+    }
+
+    public void KillMotors() {
+
+        Motor0Power = 0;
+        Motor1Power = 0;
+        Motor2Power = 0;
+        Motor3Power = 0;
+        Motor0.setPower(Motor0Power);
+        Motor1.setPower(Motor1Power);
+        Motor2.setPower(Motor2Power);
+        Motor3.setPower(Motor3Power);
+
+    }
+
+
+    public void drive(int timeval, double power, int timeafterval, boolean angleDrive, String direction) {
+
+        int choice = 0;
+
+        if (direction == "left") {
+            choice = 1;
+        } else if (direction == "right") {
+            choice = 2;
+        } else if (direction == "forward") {
+            choice = 3;
+        } else if (direction == "back") {
+            choice = 4;
+        } else {
+            telemetry.addLine("You can not have a null direction");
+            telemetry.update();
+        }
+
+        Motor0Power = power;
+        Motor1Power = power;
+        Motor2Power = power;
+        Motor3Power = power;
+
+        if (angleDrive == false) {
+            switch (choice) {
+                case 1:
+                    Motor0.setPower(Motor0Power);
+                    Motor1.setPower(-Motor1Power);
+                    Motor2.setPower(Motor2Power);
+                    Motor3.setPower(-Motor3Power);
+                    telemetryStatus(1);
+                    sleep(timeval);
+                    break;
+                case 2:
+                    Motor0.setPower(-Motor0Power);
+                    Motor1.setPower(Motor1Power);
+                    Motor2.setPower(-Motor2Power);
+                    Motor3.setPower(Motor3Power);
+                    telemetryStatus(2);
+                    sleep(timeval);
+                    break;
+                case 3:
+                    Motor0.setPower(Motor0Power);
+                    Motor1.setPower(Motor1Power);
+                    Motor2.setPower(-Motor2Power);
+                    Motor3.setPower(-Motor3Power);
+                    telemetryStatus(3);
+                    sleep(timeval);
+                    break;
+                case 4:
+                    Motor0.setPower(-Motor0Power);
+                    Motor1.setPower(-Motor1Power);
+                    Motor2.setPower(Motor2Power);
+                    Motor3.setPower(Motor3Power);
+                    telemetryStatus(4);
+                    sleep(timeval);
+                    break;
+            }
+        }
+        if (angleDrive == true){
+            switch (choice){
+                case 1:
+                    Motor0.setPower(Motor0Power);
+                    Motor1.setPower(-Motor1Power);
+                    telemetryStatus(5);
+                    sleep(timeval);
+                    break;
+                case 2:
+                    Motor0.setPower(-Motor0Power);
+                    Motor1.setPower(Motor1Power);
+                    telemetryStatus(6);
+                    sleep(timeval);
+                    break;
+                case 3:
+                    Motor2.setPower(Motor2Power);
+                    Motor3.setPower(-Motor3Power);
+                    telemetryStatus(7);
+                    sleep(timeval);
+                    break;
+                case 4:
+                    Motor2.setPower(-Motor2Power);
+                    Motor3.setPower(Motor3Power);
+                    telemetryStatus(8);
+                    sleep(timeval);
+                    break;
+            }
+        }
+        KillMotors();
+        sleep(timeafterval);
+    }
+    //Not Intended to be used anymore (Use at your own risk)
     public void MovementX(int timeval, double power, boolean direction) {
         if (direction == true) {
             Motor0Power = power;
@@ -59,7 +197,6 @@ public class AutoDrive360Test extends OpMode {
             sleep(timeval);
         }
     }
-
 
     public void MovementY(int timeval, double power, boolean direction) {
         if (direction == true) {
@@ -115,12 +252,16 @@ public class AutoDrive360Test extends OpMode {
 
     @Override
     public void loop() {
-    //Y direction format //// True = left, False = Right
+    //Don't Delete This. Strings in Java are super clunky without them
+    //Possible directions
+        String left = "left";
+        String right = "right";
+        String forward = "forward";
+        String back = "back";
+    //Write Drive Code Here
+    //Syntax for Drive Function == drive(timeval (int), power (double), aftertimeval (int), angleDrive (boolean), direction (string);
 
-        MovementX(200, .5, true);
-        MovementY(200, .5, true);
-        MovementX(200, .5, false);
-        MovementY(200, .5, false);
+        drive(500, 1, 500, false, left);
 
     }
 }
